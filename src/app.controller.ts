591d5b47d4cdfc2afba4507627ff1efca5bac414
Petrik Diak <diak@petrik.hu>
2024-09-20 16:41:48 +0200

    egyelore kesz

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,29 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { quotes } from './quotes';
+
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  private randomquote = quotes; 
 
-  @Get()
-  @Render('index')
-  getHello() {
+  @Get('quotes')
+  @Render('quotes')
+  getQuotes() {
     return {
-      message: this.appService.getHello()
+      quotes: quotes
+      }
     };
+
+
+  @Get('randomQuote')
+  @Render('randomQuote')
+  getRandomQuotes(){
+      return {
+        idezet : this.randomquote.quotes[Math.floor(Math.random() * this.randomquote.quotes.length)]
+      }
   }
-}
+  }
+  
